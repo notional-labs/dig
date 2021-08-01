24579d90b3423090c2df8e802bb230eff7075965
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-02 02:37:10 +0700

    Airdrop web server builds, need to wire up crypto and eth queries

--- airdrop/main.go
+++ airdrop/main.go
@@ -1,65 +1,67 @@
 package main
 
 import (
-    "bytes"
-    "crypto/ecdsa"
-    "fmt"
-    "log"
+	"bytes"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/ethereum/go-ethereum/common/hexutil"
-    "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	sw "github.com/faddat/dig/airdrop/go"
 )
 
 func main() {
-    
-    log.Printf("Server started")
+
+	log.Printf("Server started")
 
 	router := sw.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-    
-    privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
-    if err != nil {
-        log.Fatal(err)
-    }
 
-    publicKey := privateKey.Public()
-    publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-    if !ok {
-        log.Fatal("error casting public key to ECDSA")
-    }
+	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
 
-    data := []byte("hello")
-    hash := crypto.Keccak256Hash(data)
-    fmt.Println(hash.Hex()) // 0x1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
 
-    signature, err := crypto.Sign(hash.Bytes(), privateKey)
-    if err != nil {
-        log.Fatal(err)
-    }
+	data := []byte("hello")
+	hash := crypto.Keccak256Hash(data)
+	fmt.Println(hash.Hex()) // 0x1c8aff950685c2ed4bc3174f3472287b56d9517b9c948127319a09a7a36deac8
 
-    fmt.Println(hexutil.Encode(signature)) // 0x789a80053e4927d0a898db8e065e948f5cf086e32f9ccaa54c1908e22ac430c62621578113ddbb62d509bf6049b8fb544ab06d36f916685a2eb8e57ffadde02301
+	signature, err := crypto.Sign(hash.Bytes(), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    sigPublicKey, err := crypto.Ecrecover(hash.Bytes(), signature)
-    if err != nil {
-        log.Fatal(err)
-    }
+	fmt.Println(hexutil.Encode(signature)) // 0x789a80053e4927d0a898db8e065e948f5cf086e32f9ccaa54c1908e22ac430c62621578113ddbb62d509bf6049b8fb544ab06d36f916685a2eb8e57ffadde02301
 
-    matches := bytes.Equal(sigPublicKey, publicKeyBytes)
-    fmt.Println(matches) // true
+	sigPublicKey, err := crypto.Ecrecover(hash.Bytes(), signature)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    sigPublicKeyECDSA, err := crypto.SigToPub(hash.Bytes(), signature)
-    if err != nil {
-        log.Fatal(err)
-    }
+	matches := bytes.Equal(sigPublicKey, publicKeyBytes)
+	fmt.Println(matches) // true
 
-    sigPublicKeyBytes := crypto.FromECDSAPub(sigPublicKeyECDSA)
-    matches = bytes.Equal(sigPublicKeyBytes, publicKeyBytes)
-    fmt.Println(matches) // true
+	sigPublicKeyECDSA, err := crypto.SigToPub(hash.Bytes(), signature)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    signatureNoRecoverID := signature[:len(signature)-1] // remove recovery id
-    verified := crypto.VerifySignature(publicKeyBytes, hash.Bytes(), signatureNoRecoverID)
-    fmt.Println(verified) // true
+	sigPublicKeyBytes := crypto.FromECDSAPub(sigPublicKeyECDSA)
+	matches = bytes.Equal(sigPublicKeyBytes, publicKeyBytes)
+	fmt.Println(matches) // true
+
+	signatureNoRecoverID := signature[:len(signature)-1] // remove recovery id
+	verified := crypto.VerifySignature(publicKeyBytes, hash.Bytes(), signatureNoRecoverID)
+	fmt.Println(verified) // true
 }
