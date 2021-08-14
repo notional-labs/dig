a18cb9ec6478943e23b22074c7cb3675d7c2d554
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-15 00:52:41 +0700

    add loony node

--- auto-add-genesis-account.sh
+++ auto-add-genesis-account.sh
@@ -2,6 +2,6 @@
 for filename in ~/dig/networks/testnet-2/gentx/*
 do
     acc=$(cat $filename | jq '.body.messages[0] .delegator_address' | cut -d '"' -f 2)
-    digd add-genesis-account $acc 100000000udig
+    digd add-genesis-account $acc 150000000udig
 done
 
