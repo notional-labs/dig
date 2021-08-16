6c63d2f03ef83be81ce0a350e01dd8a547e9af7a
romelukaku <50263489+romelukaku@users.noreply.github.com>
2021-08-16 18:07:23 +0700

    Update auto-add-genesis-account.sh

--- auto-add-genesis-account.sh
+++ auto-add-genesis-account.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in ~/dig/networks/testnet-2/gentx/*
+for filename in ~/.dig/config/gentx/*
 do
     acc=$(cat $filename | jq '.body.messages[0] .delegator_address' | cut -d '"' -f 2)
     digd add-genesis-account $acc 100000000udig
