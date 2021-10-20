d472a0b38dfda0c44e9a17a9ff77087838f9182b
Brenn Hill <brennhill@gmail.com>
2021-10-20 20:01:22 +0700

    Keplr integration

--- vue/src/main.js
+++ vue/src/main.js
@@ -7,3 +7,55 @@ import vueLib from '@starport/vue'
 const app = createApp(App)
 app.config.globalProperties._depsLoaded = true
 app.use(store).use(router).use(vueLib).mount('#app')
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    async function connect() {
+        await window.keplr.experimentalSuggestChain({
+            chainId: "dig-4",
+            chainName: "Dig",
+            rpc: "http://165.232.167.156:26657",
+            rest: "http://165.232.167.156:1317",
+            bip44: {
+                coinType: 118,
+            },
+            bech32Config: {
+                bech32PrefixAccAddr: "dig",
+                bech32PrefixAccPub: "dig" + "pub",
+                bech32PrefixValAddr: "dig" + "valoper",
+                bech32PrefixValPub: "dig" + "valoperpub",
+                bech32PrefixConsAddr: "dig" + "valcons",
+                bech32PrefixConsPub: "dig" + "valconspub",
+            },
+            currencies: [
+                {
+                    coinDenom: "DIG",
+                    coinMinimalDenom: "udig",
+                    coinDecimals: 6,
+                    coinGeckoId: "dig",
+                },
+            ],
+            feeCurrencies: [
+                {
+                    coinDenom: "DIG",
+                    coinMinimalDenom: "udig",
+                    coinDecimals: 6,
+                    coinGeckoId: "dig",
+                },
+            ],
+            stakeCurrency: {
+                coinDenom: "DIG",
+                coinMinimalDenom: "udig",
+                coinDecimals: 6,
+                coinGeckoId: "dig",
+            },
+            coinType: 118,
+            gasPriceStep: {
+                low: 0.01,
+                average: 0.025,
+                high: 0.03,
+            },
+        });
+    }
+    connect();
+
+});
