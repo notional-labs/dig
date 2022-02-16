fe6775f22d4ea3cb4d2bdd6f9005c512fd71e48e
vuong <nguyenvuong1122000@gmail.com>
2022-02-16 19:20:27 +0700

    simulator

--- simapp/sim_test.go
+++ simapp/sim_test.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Profile with:
-// /usr/local/go/bin/go test -benchmem -run=^$ github.com/osmosis-labs/osmosis/simapp -bench ^BenchmarkFullAppSimulation$ -Commit=true -cpuprofile cpu.out
+// /usr/local/go/bin/go test -benchmem -run=^$ github.com/notional-labs/dig/v2/simapp -bench ^BenchmarkFullAppSimulation$ -Commit=true -cpuprofile cpu.out
 func BenchmarkFullAppSimulation(b *testing.B) {
 	// -Enabled=true -NumBlocks=1000 -BlockSize=200 \
 	// -Period=1 -Commit=true -Seed=57 -v -timeout 24h
