7a6cb7ef0fff2493ead7b2631f88729f79817735
GNaD <cpt.meoz@gmail.com>
2022-06-29 18:12:49 +0700

    Dang add proto generate scripts

---
+++ scripts/protogen.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# get protoc executions
+go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+
+# get cosmos sdk from github
+go get github.com/cosmos/cosmos-sdk 2>/dev/null
+
+echo "Generating gogo proto code"
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package $file &>/dev/null; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
+done
+
+
+# move proto files to the right places
+#
+# Note: Proto files are suffixed with the current binary version.
+cp -r github.com/faddat/dig/* ./
+rm -rf github.com
+
+go mod tidy -compat=1.18
